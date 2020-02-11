cf99c31fb44fd93f4ae61fb87c63a449d91c1cf3
kaoru2010 <kaoru2010@users.noreply.github.com>
2020-02-11 19:36:24 +0900

    数値のパースに対応

--- src/main/java/markdown/table/parser/RowWithHeaderDecoder.kt
+++ src/main/java/markdown/table/parser/RowWithHeaderDecoder.kt
@@ -30,6 +30,36 @@ class RowWithHeaderDecoder(
         return value !in arrayOf("-", "−", "/")
     }
 
+    override fun decodeTaggedInt(tag: String): Int {
+        val value = getValue(tag)
+        return value.toInt()
+    }
+
+    override fun decodeTaggedLong(tag: String): Long {
+        val value = getValue(tag)
+        return value.toLong()
+    }
+
+    override fun decodeTaggedByte(tag: String): Byte {
+        val value = getValue(tag)
+        return value.toByte()
+    }
+
+    override fun decodeTaggedFloat(tag: String): Float {
+        val value = getValue(tag)
+        return value.toFloat()
+    }
+
+    override fun decodeTaggedDouble(tag: String): Double {
+        val value = getValue(tag)
+        return value.toDouble()
+    }
+
+    override fun decodeTaggedShort(tag: String): Short {
+        val value = getValue(tag)
+        return value.toShort()
+    }
+    
     private fun getValue(tag: String): String {
         val index = requireNotNull(headerMap[tag])
         return row[index]
